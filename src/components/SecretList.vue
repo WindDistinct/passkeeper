148f88be88f573ac812d20ea87f9d1e373b92513
Nicolas Acevedo <ao.nicolasr@gmail.com>
2026-04-19 15:23:40 -0500

    Add SecretList.vue component

---
+++ src/components/SecretList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+      <SecretCard
+        v-for="secret in store.filteredSecrets"
+        :key="secret.id"
+        :item="secret"
+      />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { useSecretStore } from '../stores/secretStore'
+  import SecretCard from './SecretCard.vue'
+  
+  const store = useSecretStore()
+  </script>
+  
