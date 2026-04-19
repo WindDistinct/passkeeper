71b6390fe9139d3f5c58cee90f60e9b6475f4ea4
Nicolas Acevedo <ao.nicolasr@gmail.com>
2026-04-19 14:59:15 -0500

    Add main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h-screen bg-zinc-950 text-white flex">
+      <AppSidebar />
+  
+      <div class="flex-1 flex flex-col">
+        <AppTopbar />
+  
+        <main class="p-6">
+          <slot />
+        </main>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import AppSidebar from '../components/AppSidebar.vue'
+  import AppTopbar from '../components/AppTopbar.vue'
+  </script>
+  
