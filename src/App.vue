78ce3c4d2125f83179119be916dad2e8d5da274a
Nicolas Acevedo <ao.nicolasr@gmail.com>
2026-04-19 15:12:12 -0500

    Update tailwind class

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <MainLayout>
-    <div class="border border-dashed border-zinc-800 rounded-2xl h-full min-h-[500px] p-6">
+    <div class="border border-dashed border-zinc-800 rounded-2xl h-full min-h-125 p-6">
       <h2 class="text-2xl font-semibold mb-2">Dashboard</h2>
       <p class="text-zinc-400">Your secrets will appear here.</p>
     </div>
