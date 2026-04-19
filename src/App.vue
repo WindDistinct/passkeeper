9ca9feed9c1fb790138b5e5463e15e3dded95876
Nicolas Acevedo <ao.nicolasr@gmail.com>
2026-04-19 15:25:55 -0500

    Add secret list to MainLayout.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <MainLayout>
     <div class="border border-dashed border-zinc-800 rounded-2xl h-full min-h-125 p-6">
-      <h2 class="text-2xl font-semibold mb-2">Dashboard</h2>
-      <p class="text-zinc-400">Your secrets will appear here.</p>
+      <h2 class="text-2xl font-semibold mb-4">Your Secrets</h2>
+      <!-- <p class="text-zinc-400">Your secrets will appear here.</p> -->
+      <SecretList />
     </div>
   </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from './layouts/MainLayout.vue'
+import SecretList from './components/SecretList.vue'
 </script>
