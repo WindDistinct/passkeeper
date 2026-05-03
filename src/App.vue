ea570900d5dfb7ce470a5a709b77eb367a80187c
Nicolas Acevedo <ao.nicolasr@gmail.com>
2026-05-03 16:28:39 -0500

    Implement security placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <MainLayout @new-secret="open = true">
-    <div class="border border-dashed border-zinc-800 rounded-2xl h-full min-h-125 p-6">
-      <h2 class="text-2xl font-semibold mb-4">Your Secrets</h2>
-      <!-- <p class="text-zinc-400">Your secrets will appear here.</p> -->
-      <SecretList />
-    </div>
-  </MainLayout>
+  <UnlockPage v-if="!vault.unlocked" />
 
-  <CreateSecretModal
-    :open="open"
-    @close="open = false"
-  />
+  <template v-else> 
+    <MainLayout @new-secret="open = true">
+      <div class="border border-dashed border-zinc-800 rounded-2xl h-full min-h-125 p-6">
+        <h2 class="text-2xl font-semibold mb-4">Your Secrets</h2>
+        <!-- <p class="text-zinc-400">Your secrets will appear here.</p> -->
+        <SecretList />
+      </div>
+    </MainLayout>
+
+    <CreateSecretModal :open="open" @close="open = false" />
+  </template>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import { onMounted } from 'vue'
-  import { useSecretStore } from './stores/secretStore'
-  import MainLayout from './layouts/MainLayout.vue'
-  import SecretList from './components/SecretList.vue'
-  import CreateSecretModal from './components/CreateSecretModal.vue'
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+import { useVaultStore } from './stores/vaultStore'
+import { useSecretStore } from './stores/secretStore'
 
-  const open = ref(false)
+import UnlockPage from './pages/UnlockPage.vue'
+import MainLayout from './layouts/MainLayout.vue'
+import SecretList from './components/SecretList.vue'
+import CreateSecretModal from './components/CreateSecretModal.vue'
 
-  const secretStore = useSecretStore()
+const open = ref(false)
 
-  onMounted(async () => {
-    await secretStore.loadSecrets()
-  })
+const vault = useVaultStore()
+const secretStore = useSecretStore()
+
+onMounted(async () => {
+  await secretStore.loadSecrets()
+})
 
 </script>
